6b32d958a0f12199f267a7850ab02d9ab2a32017
Ivan Antipov <145357049+ivanio45@users.noreply.github.com>
2024-12-13 15:29:19 +0500

    Add files via upload

---
+++ src/main/java/bot_package/User.java
@@ -0,0 +1,39 @@
+package bot_package;
+
+import non_bot_package.CartManager;
+import non_bot_package.ClothingItem;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private int indPage;
+    private final CartManager cart;
+    private List<ClothingItem> clothingItems;
+
+    User(){
+        this.indPage=0;
+        this.cart = new CartManager();
+        this.clothingItems = new ArrayList<>();
+    }
+
+
+    public int getIndPage() {
+        return indPage;
+    }
+
+    public void setIndPage(int indPage) {
+        this.indPage = indPage;
+    }
+
+    public CartManager getCart() {
+        return cart;
+    }
+
+    public List<ClothingItem> getClothingItems() {
+        return clothingItems;
+    }
+
+    public void setClothingItems(List<ClothingItem> clothingItems) {
+        this.clothingItems = clothingItems;
+    }
+}
